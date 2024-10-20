dd15df7c8525bc5b5d37ef6ca17157b614e8d0c2
laith abufadda <laith.abufadda@Eranovel.local>
2024-10-20 16:36:57 +0300

    commit

--- backend/controllers/category.controller.js
+++ backend/controllers/category.controller.js
@@ -1,6 +1,7 @@
 const Joi = require("joi")
 const createError = require("../utils/createError")
 const Category = require("../models/Category")
+const Transaction = require("../models/Transaction")
 
 
 
@@ -62,7 +63,9 @@ const getUserCategories = async (req , res , next) => {
 
         const skip = (page - 1) * limit
 
-        const userCategories = await Category.find({user : req.user._id}).skip(skip).limit(limit)
+        const userCategories = await Category.find({user : req.user._id})
+            .skip(skip)
+            .limit(limit)
 
         res.status(200).json({
             userCategories ,
@@ -80,10 +83,45 @@ const getUserCategories = async (req , res , next) => {
 
 const updateCategory = async (req , res , next) => {
 
+    const {categoryId} = req.params
+    
+    const updateCategorySchema = Joi.object({
+        name : Joi.string().min(3).optional() ,
+        type: Joi.string().valid("income" , "expense").optional(),
+    })
+
+    const {value , error} = updateCategorySchema.validate(req.body , {abortEarly : false})
+
+    if(error){
+        return next(createError("Invalid Category credentials" , 400))
+    }
+
     try {
         
+        const {type , name} = value
+        const normalizedName = name?.toLowerCase()
+
+        const category = await Category.findById(categoryId)
+
+        if(!category){
+            return next(createError("Category not exist" , 404))
+        }
+
+        if(category.user.toString() !== req.user._id.toString()){
+            return next(createError("you don't have permission to update this category" , 400))
+        }
+
+        const updatedCategory = await Category.findOneAndUpdate({_id : categoryId , user : req.user._id} , {
+            $set : {
+                name : normalizedName ,
+                type
+            }
+        } , {new : true})
+
+        res.status(200).json(updatedCategory)
+
     } catch (error) {
-        
+        next(error)
     }
 
 }
@@ -93,10 +131,34 @@ const updateCategory = async (req , res , next) => {
 
 const deleteCategory = async (req , res , next) => {
 
+    const {categoryId} = req.params
+
     try {
         
+        const category = await Category.findById(categoryId)
+
+        if(!category){
+            return next(createError("Category not exist" , 404))
+        }
+
+        if(category.user.toString() !== req.user._id.toString()){
+            return next(createError("you don't have permission to delete this category" , 400))
+        }
+
+        const defaultCategory = "Uncategorized"
+
+        await Transaction.updateMany({user : req.user._id , category : category.name} , {
+            $set : {
+                category : defaultCategory
+            }
+        })
+
+        await Category.findByIdAndDelete(categoryId)
+
+        res.status(200).json({msg : "category deleted successfully"})
+
     } catch (error) {
-        
+        next(error)
     }
 
 }
